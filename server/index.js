eb9ecda6fbbcedfadacf109783e4fa0f3ecb9d9e
DoubleU23 <sf@tailored-digital.com>
2016-02-16 17:49:39 +0100

    basic stack env + webpackConfig, hot-reaload-server, ...

---
+++ server/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+'use strict';
+const path = require('path');
+
+require('babel-register')({
+	ignore: new RegExp(path.resolve(__dirname, '../node_modules'))
+});
+require('babel-polyfill');
+
+require('./server.js');
