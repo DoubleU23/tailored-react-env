c3d55b7b7559d4d780abf2eed87bbfb6ba430ce0
DoubleU23 <sf@tailored-digital.com>
2017-06-30 13:40:15 +0200

    prevent: "Error: only one instance of babel-polyfill is allowed"

--- __tests__/karma.entry.js
+++ __tests__/karma.entry.js
@@ -35,9 +35,10 @@ global = Object.assign(global,
     {mount, render, shallow},                           // enzyme render functions
     {chai, expect: chai.expect, should: chai.should()}  // chai assertion functions
 )
-
-
-require('babel-polyfill')
+// prevent: "Error: only one instance of babel-polyfill is allowed"
+if (!global._babelPolyfill) {
+    require('babel-polyfill')
+}
 /**
  * LOAD FILES
  *     according to https://github.com/webpack-contrib/istanbul-instrumenter-loader#testindexjs
