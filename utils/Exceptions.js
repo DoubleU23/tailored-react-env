d24343c5e631eff2aa43fe4d48c429d5984bc34a
DoubleU23 <sf@tailored-digital.com>
2017-07-19 14:31:28 +0200

    cleaned up Exceptions

--- utils/Exceptions.js
+++ utils/Exceptions.js
@@ -18,34 +18,7 @@ class ExtendableError extends Error {
 
 }
 
-/**
- *  Class APIError
- *  to throw an Error for bad API Responses (not 200)
- *  gets error msg from 'intl' with backup to Responses statusText
- *
- *  @dependecy {function} getState (got it from the action)
- *  @usedBy async API
- */
-export class APIError extends ExtendableError {
-
-    /**
-     * APIError::constructor
-     * @param  {Response}   Response - a Response Object (status)
-     * @param  {function}   getState (got it from the action)
-     */
-    constructor(message, additionalData) {
-        super(message)
-
-        this.message = message
-        this.type = additionalData.type || 'API'
-        this.name = 'APIError'
-        this.error = true
-        additionalData.forEach((data, index) => {
-            this[index] = data
-        })
-    }
-
-}
+// TBD: APIError
 
 /**
  *  class TimeoutError
@@ -62,46 +35,6 @@ export class TimeoutError extends ExtendableError {
         super(msg)
         this.name  = 'TimeoutError'
         this.type  = 'timeout'
-        this.error = true
     }
 
 }
-
-// CUSTOM ERROR for custom "fetchAPI"
-// will be used as blueprint for network Error
-/*
-export const throwAPIError = (response, getState, type = 'API') => {
-    // type = 'API' || 'session'
-
-    // typeError?
-    if (response instanceof Error) {
-        console.log('[throwAPIError] response instanceof Error',  response)
-
-        throw response
-    }
-
-    // preset msg
-    let message
-    if (typeof getState === 'function') {
-        let state    = getState(),
-            {intl}   = state,
-            messages = intl instanceof Record
-                            ? intl.get('messages')[intl.get('selectedLanguage')]
-                     : intl.messages[intl.selectedLanguage] // not needed anymore // kept as fallback
-
-        console.log('messages', messages)
-
-        message      = messages.forms.errors[type][response.status]
-                        || response.statusText
-    }
-    else {
-        message      = response.statusText || 'API call failed.'
-    }
-
-    const error      = new APIError(message, {response, type})
-
-    console.log('Error: ', error)
-
-    throw error
-}
-*/
