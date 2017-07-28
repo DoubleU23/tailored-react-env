feada1903c4315e527a291daf3747dcf28785920
sfriedl <sf@tailored-digital.com>
2017-07-28 23:16:25 +0200

    fixxed testServer entryPoint

--- stack/server/test/index.js
+++ stack/server/test/index.js
@@ -4,7 +4,7 @@
 const path = require('path')
 
 require('babel-register')({
-    ignore: new RegExp(path.resolve(__dirname, '../../../node_modules'))
+    ignore: path.resolve(__dirname, '../../../node_modules')
 })
 require('babel-polyfill')
 
