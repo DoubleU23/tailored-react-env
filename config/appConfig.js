f18de009c8273923dd660759928b6db224a43de4
DoubleU23 <sf@tailored-digital.com>
2017-08-01 12:38:37 +0200

    added /static/**/* to config.globs

--- config/appConfig.js
+++ config/appConfig.js
@@ -39,7 +39,8 @@ export const getAppConfig = _isDevelopment => {
             nodeModules:    appPaths.nodeModules   + '/**',
             testFiles:      appPaths.tests         + '/**/*.test.{js,jsx}',
             coverage:       appPaths.coverage      + '/**/*',
-            build:          appPaths.build         + '/**/*'
+            build:          appPaths.build         + '/**/*',
+            static:         appPaths.static         + '/**/*'
         },
 
         api, // from 'config'
