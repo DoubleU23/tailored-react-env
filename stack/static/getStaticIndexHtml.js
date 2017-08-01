0c55a2956fab562484481ca885b272fcf5ae0d60
DoubleU23 <sf@tailored-digital.com>
2017-08-01 12:51:28 +0200

    added getStaticIndexHtml({js, css})

---
+++ stack/static/getStaticIndexHtml.js
@@ -0,0 +1,16 @@
+export default function getStaticIndexHtml({js, css}) {
+    return `<!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta http-equiv="content-type" content="text/html; charset=utf-8">
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+                <link rel="stylesheet" href="./${css}">
+            </head>
+            <body>
+                <div id="app"></div>
+                <script type="text/javascript" src="./${js}"></script>
+            </body>
+        </html>
+        `
+}
